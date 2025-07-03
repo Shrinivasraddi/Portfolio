edd7cb2c24f83ab6aee2e444197d31a4432ab48e
SHRINIVASRADDI R G <shrinivasraddirg@gmail.com>
2025-07-03 21:19:51 +0530

    converted to ejs files

--- script.js
+++
@@ -1,9 +0,0 @@
-function sendMail(){
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    }
-
-    emailjs.send("service_89jrxl6","template_32ebo0c",parms).then(alert("Email Sent!!"));
-}
