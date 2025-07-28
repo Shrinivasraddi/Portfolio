1976873d46f23461f1609b2a31300defb4123e33
SHRINIVASRADDI R G <shrinivasraddirg@gmail.com>
2025-07-28 15:39:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const toggle = document.getElementById('nav-toggle');
   const nav = document.getElementById('nav-menu');
+  const navLinks = document.querySelectorAll('.nav__link');
 
   if (toggle && nav) {
     toggle.addEventListener('click', () => {
       nav.classList.toggle('show');
+    });
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        nav.classList.remove('show');
+      });
     });
   }
 });
